95a1c24981e8434b4ba5a2fa253e15e2524b27bb
zephyrus21 <pandey.piyus@gmail.com>
2020-10-18 14:33:25 +0530

    adding photo

--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 const GameOverScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text>The Game is Over!</Text>
+            <Image
+                style={styles.image}
+                source={require('../assets/success.png')}
+                resizeMode="cover"
+            />
             <Text>Number of rounds: {props.roundsNumber}</Text>
             <Text>Number was: {props.userNumber}</Text>
             <Button title="NEW GAME" onPress={props.onRestart} />
@@ -17,6 +22,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderRadius: 20,
+        marginVertical: 50,
     },
 });
 
